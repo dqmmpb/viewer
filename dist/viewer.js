abae36203012074ce6544d6cfa4b641aec5ffdd6
邓启明 <dqmmpb@163.com>
2016-12-14 12:28:51 +0800

    fixed: viewer-action css

--- dist/viewer.js
+++ dist/viewer.js
@@ -1879,17 +1879,17 @@
       '<div class="viewer-footer">' +
         '<div class="viewer-title"></div>' +
         '<ul class="viewer-toolbar">' +
-          '<li class="viewer-zoom-in" data-action="zoom-in"></li>' +
-          '<li class="viewer-zoom-out" data-action="zoom-out"></li>' +
-          '<li class="viewer-one-to-one" data-action="one-to-one"></li>' +
-          '<li class="viewer-reset" data-action="reset"></li>' +
-          '<li class="viewer-prev" data-action="prev"></li>' +
-          '<li class="viewer-play" data-action="play"></li>' +
-          '<li class="viewer-next" data-action="next"></li>' +
-          '<li class="viewer-rotate-left" data-action="rotate-left"></li>' +
-          '<li class="viewer-rotate-right" data-action="rotate-right"></li>' +
-          '<li class="viewer-flip-horizontal" data-action="flip-horizontal"></li>' +
-          '<li class="viewer-flip-vertical" data-action="flip-vertical"></li>' +
+          '<li class="viewer-action viewer-zoom-in" data-action="zoom-in"></li>' +
+          '<li class="viewer-action viewer-zoom-out" data-action="zoom-out"></li>' +
+          '<li class="viewer-action viewer-one-to-one" data-action="one-to-one"></li>' +
+          '<li class="viewer-action viewer-reset" data-action="reset"></li>' +
+          '<li class="viewer-action viewer-prev" data-action="prev"></li>' +
+          '<li class="viewer-action viewer-play" data-action="play"></li>' +
+          '<li class="viewer-action viewer-next" data-action="next"></li>' +
+          '<li class="viewer-action viewer-rotate-left" data-action="rotate-left"></li>' +
+          '<li class="viewer-action viewer-rotate-right" data-action="rotate-right"></li>' +
+          '<li class="viewer-action viewer-flip-horizontal" data-action="flip-horizontal"></li>' +
+          '<li class="viewer-action viewer-flip-vertical" data-action="flip-vertical"></li>' +
         '</ul>' +
         '<div class="viewer-navbar">' +
           '<ul class="viewer-list"></ul>' +
